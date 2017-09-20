4356304283726cbdcb9714d6e73e59ed414c9f03
papagalu <dmititelu@cloudbasesolutions.com>
2017-09-20 19:45:35 +0300

    Playing with zmq

---
+++ zmq/server.py
@@ -0,0 +1,14 @@
+import zmq
+import time
+
+context = zmq.Context()
+socket = context.socket(zmq.REP)
+socket.bind("tcp://*:5555")
+
+while True:
+    message = socket.recv()
+    print("Received request: %s" % message)
+
+    time.sleep(1)
+
+    socket.send(b"work")
